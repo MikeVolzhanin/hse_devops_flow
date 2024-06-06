e2801e9900e22a0ef8a3980608170de66bc3a75f
Mikhail Volzhanin <mishavolzhanin.mishavolzhanin2@gmail.com>
2024-06-06 12:49:16 +0300

    // Метод для нахождения наибольшего общего делителя

---
+++ src/EuclideanAlgorithm.java
@@ -0,0 +1,13 @@
+public class EuclideanAlgorithm {
+    // Метод для нахождения наибольшего общего делителя
+    public static int gcd(int a, int b) {
+        // Применяем алгоритм Евклида
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        // На этом этапе 'a' содержит НОД
+        return a;
+    }
+}
